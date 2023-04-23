92bfd64d6499e9001b36c538ea8ae79c3d1469c0
Prithviraj Khelkar <pkhelkar26@gmail.com>
2023-04-23 13:14:16 -0400

    update git submodules

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+#include <tuple>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <set>
+#include <queue>
+#include <sstream>
+#include "polyscope/polyscope.h"
+#include "polyscope/surface_mesh.h"
+
+using namespace std;
+
+polyscope::SurfaceMesh *psMesh;
+
+
+int main(int argc, char **argv) {
+
+  std::string filename = argv[1];
+  std::ifstream in(filename);
+
+  std::vector<std::vector<double>> positions;
+  std::vector<std::vector<int>> faces;
+  std::map<int, std::vector<double>> field;
+  string line;
+
+  int i = 0;
+  while( getline( in, line ))
+      {
+         std::stringstream ss( line );
+         string token;
+
+         ss >> token;
+
+         if( token == "v"  ) {
+            
+            double x, y, z;
+            ss >> x >> y >> z;
+            std::vector<double> vec{x, y, z};
+            positions.push_back(vec);
+         };
+         if( token == "f"  ) {
+            int v1, v2, v3;
+            ss >> v1 >> v2 >> v3;
+            std::vector<int> face{v1-1, v2-1, v3-1};
+            faces.push_back(face);
+         };
+         if( token == "vf"  ) {
+            double x, y, z;
+            ss >> x >> y >> z;
+            std::vector<double> vec{x, y, z};
+            field[i] = vec;
+            i++;
+         };
+      }
+
+  polyscope::init();
+
+  psMesh = polyscope::registerSurfaceMesh("mesh", positions, faces);
+
+  polyscope::getSurfaceMesh("mesh")->addVertexIntrinsicVectorQuantity("great vectors", field);
+
+  polyscope::show();
+
+  return 0;
+}
